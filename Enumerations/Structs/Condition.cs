e0605278afc6117b56e29be942265f5091571dd4
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-03-17 17:26:16 -0400

    Add Async Support for Conditions and Consequences

--- Enumerations/Structs/Condition.cs
+++ Enumerations/Structs/Condition.cs
@@ -9,16 +9,19 @@ public readonly record struct Condition<TKey, TEnumeration>
     where TKey : IEnumerationBase
     where TEnumeration : IEquatable<TEnumeration>, IComparable<TEnumeration>
 {
+    #region Private Members
     private readonly IEnumerationBase _enumerationBase;
 
     private readonly bool _isEvaluationStopped;
-
+    #endregion
+    #region Constructors
     internal Condition(Boolean stopEvaluation, IEnumerationBase enumerationBase)
     {
         _enumerationBase = enumerationBase;
         _isEvaluationStopped = stopEvaluation;
     }
-
+    #endregion
+    #region Default Actions and Conditions
     /// <summary>
     /// Executable action when no other calls to this consequence have been matched
     /// </summary>
@@ -30,6 +33,39 @@ public readonly record struct Condition<TKey, TEnumeration>
             action();
         }
     }
+
+    /// <summary>
+    /// Executable <paramref name="action" /> that can be run using <see langword="async"/>...<seealso langword="await"/> lambda expressions
+    /// </summary>
+    /// <param name="action">The action we want to run</param>
+    /// <remarks><paramref name="action"/> is called with <c>ConfigureAwait(false)</c></remarks>
+    public async Task DefaultAsyncCondition(Func<Task> action)
+    {
+        if (!_isEvaluationStopped)
+        {
+            await action().ConfigureAwait(false);
+        }
+    }
+
+    /// <summary>
+    /// Executable <paramref name="action" /> that can be run using <see langword="async"/>...<seealso langword="await"/> lambda expressions, with <see cref="CancellationToken"/> support
+    /// </summary>
+    /// <param name="action">The action we want to run</param>
+    /// <param name="cancellationToken"></param>
+    /// <remarks><paramref name="action"/> is called with <c>ConfigureAwait(false)</c></remarks>
+    public async Task DefaultAsyncConditionWithCancellation(Func<Task> action, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            await Task.FromCanceled(cancellationToken).ConfigureAwait(false);
+        }
+
+        if (_isEvaluationStopped)
+        {
+            await action().ConfigureAwait(false);
+        }
+    }
+    #endregion
     #region Execution Methods
     /// <summary>
     /// When the given condition is matched by the specified <see cref="EnumerationBase{TKey,TEnumeration}"/> parameters, then we retrieve the <see cref="Consequence{TKey, TEnumeration}"/>
@@ -37,7 +73,7 @@ public readonly record struct Condition<TKey, TEnumeration>
     /// <param name="enumerationBaseConditional">A supplied <see cref="EnumerationBase{TKey,TEnumeration}"/> values to compare to in this instance</param>
     /// <returns>An execution path for the supplied action</returns>
     public Consequence<TKey, TEnumeration> When(IEnumerationBase enumerationBaseConditional) =>
-        new (
+        new(
             isMatch: _enumerationBase.Equals(enumerationBaseConditional),
             stopEvaluating: _isEvaluationStopped,
             enumerationBase: _enumerationBase
@@ -48,7 +84,7 @@ public readonly record struct Condition<TKey, TEnumeration>
     /// <param name="enumerationBaseConditionals">A collection of <see cref="EnumerationBase{TKey,TEnumeration}"/> values to compare to in this instance</param>
     /// <returns>An execution path for the supplied action</returns>
     public Consequence<TKey, TEnumeration> When(IEnumerable<IEnumerationBase> enumerationBaseConditionals) =>
-        new (isMatch: enumerationBaseConditionals.Contains(_enumerationBase),
+        new(isMatch: enumerationBaseConditionals.Contains(_enumerationBase),
             stopEvaluating: _isEvaluationStopped,
             enumerationBase: _enumerationBase);
     /// <summary>
--- Enumerations/Structs/Consequence.cs
+++ Enumerations/Structs/Consequence.cs
@@ -8,19 +8,22 @@ public readonly record struct Consequence<TKey, TEnumeration>
     where TKey : IEnumerationBase
     where TEnumeration : IEquatable<TEnumeration>, IComparable<TEnumeration>
 {
+    #region Private Members
     private readonly Boolean _isMatch;
-    
-    private readonly IEnumerationBase _enumerationBase;
-    
-    private readonly Boolean _isEvaluationStopped;
 
+    private readonly IEnumerationBase _enumerationBase;
+
+    private readonly Boolean _isEvaluationStopped;
+    #endregion
+    #region Constructors
     internal Consequence(Boolean isMatch, Boolean stopEvaluating, IEnumerationBase enumerationBase)
     {
         _isMatch = isMatch;
         _isEvaluationStopped = stopEvaluating;
         _enumerationBase = enumerationBase;
     }
-
+    #endregion
+    #region Synchronous Methods
     /// <summary>
     /// Calls the supplied <paramref name="action"/> when the preceding <see cref="Condition{TKey,TEnumeration}"/> is matched
     /// </summary>
@@ -33,6 +36,42 @@ public readonly record struct Consequence<TKey, TEnumeration>
             action();
         }
 
-        return new Condition<TKey, TEnumeration>(_isEvaluationStopped || _isMatch, _enumerationBase);
+        return new(_isEvaluationStopped || _isMatch, _enumerationBase);
     }
+    #endregion
+    #region Async Support
+
+    /// <summary>
+    /// Calls the supplied <paramref name="action"/> when the preceding <see cref="Condition{TKey,TEnumeration}"/> is matched
+    /// </summary>
+    /// <param name="action">Asynchronous Action to call</param>
+    /// <returns>a chainable instance of a Conditions for more Consequences</returns>
+    /// <remarks><paramref name="action"/> is called with <c>ConfigureAwait(false)</c></remarks> 
+    public Condition<TKey, TEnumeration> ThenAwait(Func<Task> action)
+    {
+        if (!_isEvaluationStopped && _isMatch)
+        {
+            action();
+        }
+
+        return new(_isEvaluationStopped || _isMatch, _enumerationBase);
+    }
+
+    /// <summary>
+    /// Calls the supplied <paramref name="action"/> when the preceding <see cref="Condition{TKey,TEnumeration}"/> is matched
+    /// </summary>
+    /// <param name="action">Async Action to call</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>a chainable instance of a Conditions for more Consequences</returns>
+    /// <remarks><paramref name="action"/> is called with <c>ConfigureAwait(false)</c></remarks>
+    public Condition<TKey, TEnumeration> ThenAwaitWithCancellation(Func<Task> action, CancellationToken cancellationToken = default)
+    {
+        if (!cancellationToken.IsCancellationRequested || !_isEvaluationStopped && _isMatch)
+        {
+            action().ConfigureAwait(false);
+        }
+
+        return new(_isEvaluationStopped || _isMatch, _enumerationBase);
+    }
+    #endregion
 }
