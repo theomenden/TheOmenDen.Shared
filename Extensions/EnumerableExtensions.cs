1f4f6c37a5a84bd13779f77c039bd597123ce71d
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-03-24 23:47:35 -0400

    Update the Shared Library to use System.Random.Shared for thread safe random generation, remove ThreadSafeRandom class since it's purpose is redundant. Refactored Enumerable Random extensions to use a shuffle algorithm to get randomized values.

--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -3,26 +3,39 @@ using TheOmenDen.Shared.Utilities;
 
 namespace TheOmenDen.Shared.Extensions;
 
+/// <summary>
+/// Provides a set of LINQ-esque extension methods over the <see cref="IEnumerable{T}" /> type.
+/// </summary>
 public static class EnumerableExtensions
 {
-
+    /// <summary>
+    /// Instructs the <paramref name="source"/> to enact the <paramref name="action"/> upon each of its respective elements
+    /// </summary>
+    /// <typeparam name="T">The underlying type we're enumerating</typeparam>
+    /// <param name="source">The supplied enumerable</param>
+    /// <param name="action">The action we wish to enact</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null</exception>
     public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
     {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
         switch (source)
         {
-            case T[] array: Array.ForEach(array, action); break;
-            case ICollection<T> collection: collection.ForEach(action); break;
+            case T[] {Length: > 0} array: Array.ForEach(array, action);
+                break;
+            case List<T> {Count: > 0} list: list.ForEach(action);
+                break;
             default:
-                foreach (var item in source)
+                foreach (var element in source)
                 {
-                    action(item);
+                    action(element);
                 }
                 break;
         }
-
     }
-
-
 
     /// <summary>
     /// Retrieves a random value from the provided <paramref name="source"/> collection
@@ -31,231 +44,102 @@ public static class EnumerableExtensions
     /// <param name="source">The provided collection</param>
     /// <returns>A random entry <typeparamref name="T"/></returns>
     /// <exception cref="InvalidOperationException">If there's nothing in the <paramref name="source"/> collection</exception>
-    public static T GetRandomElement<T>(this IEnumerable<T> source)
+    public static T GetRandomElement<T>(this IEnumerable<T>? source)
+    => source is not null
+            ? source switch
+            {
+                List<T> { Count: > 0 } list => GetRandomElementFromList(list),
+                T[] { Length: > 0 } array => GetRandomElementFromArray(array),
+                ICollection<T> { Count: > 0 } collection => collection.ElementAt(Random.Shared.Next(collection.Count)),
+                _ => FisherYatesDurstenfeldShuffle(source.ToArray()).First()
+            }
+            : throw new ArgumentNullException(nameof(source));
+
+    /// <summary>
+    /// Access a random element from the provided <paramref name="source"/>
+    /// </summary>
+    /// <typeparam name="T">The underlying type</typeparam>
+    /// <param name="source">The provided array</param>
+    /// <returns>A single <typeparamref name="T"/> from <paramref name="source"/></returns>
+    public static T GetRandomElementFromArray<T>(this T[] source)
+    => source[Random.Shared.Next(source.Length)];
+
+    /// <summary>
+    /// Retrieves a random element from the provided <paramref name="source"/>
+    /// </summary>
+    /// <typeparam name="T">The underlying type</typeparam>
+    /// <param name="source">The provided array</param>
+    /// <returns>A single <typeparamref name="T"/> from <paramref name="source"/></returns>
+    public static T GetRandomElementFromList<T>(this List<T> source)
+    => source[Random.Shared.Next(source.Count)];
+
+    /// <summary>
+    /// Retrieves a set of elements from the provided <paramref name="source"/> collection
+    /// </summary>
+    /// <typeparam name="T">The underlying type</typeparam>
+    /// <param name="source">The provided collection</param>
+    /// <param name="totalElementsToReturn">The number of random elements we want to recieve</param>
+    /// <returns><see cref="IEnumerable{T}"/>: <typeparamref name="T"/></returns>
+    /// <exception cref="ArgumentNullException">throw when the <paramref name="source"/> is null</exception>
+    public static IEnumerable<T> GetRandomElements<T>(this IEnumerable<T> source, Int32 totalElementsToReturn)
     {
-        switch (source)
+        if (source is null)
         {
-            case ICollection<T> { Count: > 0 } collection:
-                return collection.ElementAt(ThreadSafeRandom.Global.Next(collection.Count));
-            case T[] { Length: > 0 } array:
-                return GetRandomElementFromArray(array);
-            default:
-
-                var randomElement = default(T);
-                var indexCounter = 0;
-
-                foreach (var element in source)
-                {
-                    indexCounter++;
-
-                    if (ThreadSafeRandom.Global.Next(indexCounter) is 0)
-                    {
-                        randomElement = element;
-                    }
-                }
-
-                if (indexCounter <= 0)
-                {
-                    throw new InvalidOperationException($"{nameof(source)} collection was empty");
-                }
-
-                return randomElement!;
+            throw new ArgumentNullException(nameof(source));
         }
+
+        return totalElementsToReturn <= 0
+            ? Enumerable.Empty<T>()
+            : source switch
+            {
+                ICollection<T> collection when totalElementsToReturn >= collection.Count => collection,
+                T[] array => GetRandomElementsFromArray(array, totalElementsToReturn),
+                IList<T> list => GetRandomElementsFromList(list, totalElementsToReturn),
+                _ => FisherYatesDurstenfeldShuffle(source.ToArray())
+            };
     }
 
     /// <summary>
-    /// Retrieves a random <see cref="Index" /> from the provided <paramref name="source"/>
+    /// Retrieves a random element from <paramref name="source"/> with the total <paramref name="totalElementsToReturn"/> being the number of random elements
     /// </summary>
-    /// <param name="source">The provided collection</param>
     /// <typeparam name="T">The underlying type</typeparam>
-    /// <returns>A random <see cref="Index"/></returns>
-public static Index GetRandomIndex<T>(this IEnumerable<T> source)
-{
-    var random = ThreadSafeRandom.Global.Next(source.Count() - 1);
+    /// <param name="source">The supplied array</param>
+    /// <param name="totalElementsToReturn">Number of random elements we wish to return</param>
+    /// <returns>Ann array of: <typeparamref name="T"/></returns>
+    /// <remarks>Attempts to implement a naive interpretation of the Fisher-Yates-Durstenfeld shuffling algorithm</remarks>
+    public static T[] GetRandomElementsFromArray<T>(this T[] source, Int32 totalElementsToReturn)
+    => FisherYatesDurstenfeldShuffle(source)[..totalElementsToReturn];
 
-    return GenerateRandomElementIndex(1, random);
-}
+    /// <summary>
+    /// Retrieves a random element retrieval from <paramref name="source"/> with the total <paramref name="totalElementsToReturn"/> being the number of random elements
+    /// </summary>
+    /// <typeparam name="T">The underlying type</typeparam>
+    /// <param name="source">The supplied list</param>
+    /// <param name="totalElementsToReturn">Number of random elements we wish to return</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of: <typeparamref name="T"/></returns>
+    /// <remarks>Attempts to implement a naive interpretation of the Fisher-Yates-Durstenfeld shuffling algorithm</remarks>
+    public static IEnumerable<T> GetRandomElementsFromList<T>(this IList<T>? source, Int32 totalElementsToReturn)
+    => source is not null
+            ? FisherYatesDurstenfeldShuffle(source.ToArray()).Take(totalElementsToReturn)
+            : throw new ArgumentNullException(nameof(source));
 
-/// <summary>
-/// Access a random element from the provided <paramref name="source"/>
-/// </summary>
-/// <typeparam name="T">The underlying type</typeparam>
-/// <param name="source">The provided array</param>
-/// <returns>A single <typeparamref name="T"/> from <paramref name="source"/></returns>
-public static T GetRandomElementFromArray<T>(this T[] source)
-{
-    var indexCounter = source.Length - 1;
-
-    var index = GenerateRandomElementIndex(1, ThreadSafeRandom.Global.Next(indexCounter));
-
-    return source[index];
-}
-
-/// <summary>
-/// Retrieves a set of elements from the provided <paramref name="source"/> collection
-/// </summary>
-/// <typeparam name="T">The underlying type</typeparam>
-/// <param name="source">The provided collection</param>
-/// <param name="totalElementsToReturn">The number of random elements we want to recieve</param>
-/// <returns><see cref="IEnumerable{T}"/>: <typeparamref name="T"/></returns>
-/// <exception cref="ArgumentNullException">throw when the <paramref name="source"/> is null</exception>
-public static IEnumerable<T> GetRandomElements<T>(this IEnumerable<T> source, Int32 totalElementsToReturn)
-{
-    if (source is null)
+    /// <summary>
+    /// Shuffles the provided <paramref name="source" /> using an interpretation of the Fisher-Yates-Durstenfeld shuffling algorithm
+    /// </summary>
+    /// <typeparam name="T">The type of elements we're shuffling</typeparam>
+    /// <param name="source">The provided <see cref="Array"/></param>
+    /// <returns>The shuffled array</returns>
+    /// <remarks><see href="https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm">Fisher Yates Durstenfeld Shuffle on wikipedia</see></remarks>
+    private static T[] FisherYatesDurstenfeldShuffle<T>(this T[] source)
     {
-        throw new ArgumentNullException(nameof(source));
-    }
+        var sourceSpan = source.AsSpan();
 
-    if (totalElementsToReturn <= 0)
-    {
-        return Enumerable.Empty<T>();
-    }
-
-    switch (source)
-    {
-        case ICollection<T> collection when totalElementsToReturn >= collection.Count:
-            return collection;
-        case T[] array:
-            return GetRandomElementsFromArray(array, totalElementsToReturn);
-        case IList<T> list:
-            return GetRandomElementsFromList(list, totalElementsToReturn);
-        default:
-            var randomIndex = 0;
-
-            var sourceAsArray = source.ToArray();
-            var subArray = sourceAsArray.SubArray(0, totalElementsToReturn);
-
-            foreach (var item in sourceAsArray)
-            {
-                if (randomIndex < totalElementsToReturn)
-                {
-                    subArray[randomIndex] = item;
-                    randomIndex++;
-                    continue;
-                }
-
-                var index = ThreadSafeRandom.Global.Next(randomIndex + 1);
-
-                if (index < totalElementsToReturn)
-                {
-                    subArray[index] = item;
-                }
-
-                randomIndex++;
-            }
-
-            return subArray;
-    }
-}
-
-/// <summary>
-/// Array Optimized Random element retrieval from <paramref name="source"/> with the total <paramref name="totalElementsToReturn"/> being the number of random elements
-/// </summary>
-/// <typeparam name="T">The underlying type</typeparam>
-/// <param name="source">The supplied array</param>
-/// <param name="totalElementsToReturn">Number of random elements we wish to return</param>
-/// <returns>Ann array of: <typeparamref name="T"/></returns>
-public static T[] GetRandomElementsFromArray<T>(this T[] source, Int32 totalElementsToReturn)
-{
-    var sourceSpan = source.AsSpan();
-
-    var arrayToReturn = new T[totalElementsToReturn];
-
-    for (var i = 0; i < sourceSpan.Length; i++)
-    {
-        var index = GenerateRandomElementIndex(totalElementsToReturn, i);
-
-        if (index.Value is not -1 && index.Value < totalElementsToReturn)
+        for (var i = 0; i < sourceSpan.Length - 1; i++)
         {
-            arrayToReturn[index] = source[i];
+            var randomIndex = Random.Shared.Next(i, source.Length);
+            (sourceSpan[randomIndex], sourceSpan[i]) = (sourceSpan[i], sourceSpan[randomIndex]);
         }
+
+        return sourceSpan.ToArray();
     }
-
-    return arrayToReturn;
-}
-
-/// <summary>
-/// <see cref="IList{T}"/> Optimized Random element retrieval from <paramref name="source"/> with the total <paramref name="totalElementsToReturn"/> being the number of random elements
-/// </summary>
-/// <typeparam name="T">The underlying type</typeparam>
-/// <param name="source">The supplied list</param>
-/// <param name="totalElementsToReturn">Number of random elements we wish to return</param>
-/// <returns>An array of: <typeparamref name="T"/></returns>
-public static T[] GetRandomElementsFromList<T>(this IList<T> source, Int32 totalElementsToReturn)
-{
-    var returnList = new T[totalElementsToReturn];
-
-    for (var i = 0; i < source.Count; i++)
-    {
-        var index = GenerateRandomElementIndex(totalElementsToReturn, i);
-
-        if (index.Value is not -1 && index.Value < totalElementsToReturn)
-        {
-            returnList[index] = source[i];
-        }
-    }
-
-    return returnList;
-}
-
-/// <summary>
-/// Retrieves a random set of <see cref="Index"/> from the provided <paramref name="source"/>
-/// </summary>
-/// <typeparam name="T">The underlying type</typeparam>
-/// <param name="source">The source collection</param>
-/// <param name="totalIndiciesToReturn">The total elements we wish to return</param>
-/// <returns><see cref="Array"/>:<see cref="Index"/></returns>
-public static Index[] GetRandomElementIndiciesFromArray<T>(this T[] source, Int32 totalIndiciesToReturn)
-{
-    var returnList = new Index[totalIndiciesToReturn];
-
-    for (var i = 0; i < source.Length; i++)
-    {
-        var index = GenerateRandomElementIndex(totalIndiciesToReturn, i);
-
-        if (index.Value is not -1 && index.Value < totalIndiciesToReturn)
-        {
-            returnList[index] = new(i);
-        }
-    }
-
-    return returnList;
-}
-
-/// <summary>
-/// Retrieves a random set of <see cref="Index"/> from the provided <paramref name="source"/>
-/// </summary>
-/// <typeparam name="T">The underlying type</typeparam>
-/// <param name="source">The source collection</param>
-/// <param name="totalIndiciesToReturn">The total elements we wish to return</param>
-/// <returns><see cref="Array"/>:<see cref="Index"/></returns>
-public static Index[] GetRandomElementIndiciesFromList<T>(this IList<T> source, Int32 totalIndiciesToReturn)
-{
-    var randomIndexes = new Index[totalIndiciesToReturn];
-
-    for (var i = 0; i < source.Count; i++)
-    {
-        var index = GenerateRandomElementIndex(totalIndiciesToReturn, i);
-
-        if (index.Value is not -1 && index.Value < totalIndiciesToReturn)
-        {
-            randomIndexes[index] = new(i);
-        }
-    }
-
-    return randomIndexes;
-}
-
-private static Index GenerateRandomElementIndex(int totalValuesToReturn, int randomIndex)
-{
-    if (randomIndex < totalValuesToReturn)
-    {
-        return new(randomIndex);
-    }
-
-    var index = ThreadSafeRandom.Global.Next(randomIndex + 1);
-
-    return index < totalValuesToReturn ? index : -1;
-}
-
 }
